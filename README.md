85a2c8660f3976ce7ee21a63db9d3dcc2870af7d
Francois van Heerden <vanheerden.francois20@gmail.com>
2020-02-15 14:22:20 +0200

    Major Update to Model and README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Commodity Price Forecasting
 
-Forecasting is such an essential part of business and everyday life. It is in your weather app and your business plan. I did some research and discovered that Facebook open sourced their time series forecasting model Prophet.
+Forecasting is such an essential part of business and everyday life. It is in your weather app and your business plan. I did some research and discovered that Facebook open sourced their time series forecasting model Prophet. [Read More](https://facebook.github.io/prophet/)
 
 ## Getting Started
 
@@ -19,10 +19,16 @@ pandas
 glob2
 numpy
 fbprophet
+plotly
 ```
+You might find that fbprohet does not install via pip then you can run a conda-forge:
+```
+conda install -c conda-forge fbprophet
+```
+
 ### Download the data
 
-* [Corn & Soybean Prices 2008-2017](https://www.kaggle.com/ainslie/usda-wasde-monthly-corn-soybean-projections#USDAProj_Soybean_2015to2016.csv)
+* [Time Series Minimum Temp 1981-1991](https://www.kaggle.com/shenba/time-series-datasets)
 
 ### The Model
 
@@ -41,7 +47,7 @@ import datetime as dt
 ```Python
 
 ```
-
+![Min Temp Plot]('https://www.google.com/url?sa=i&url=https%3A%2F%2Fplot.ly%2Fpython%2Fv3%2Ftime-series%2F&psig=AOvVaw1YF1xDk6r4xcAxfuIb_qYh&ust=1581855695514000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPD53unF0-cCFQAAAAAdAAAAABAD')
 ## Functions
 
 ```Python
--- model.py
+++ model.py
@@ -6,35 +6,63 @@ import glob2 as glob
 import numpy as np
 from fbprophet import Prophet
 from fbprophet.plot import add_changepoints_to_plot
+from fbprophet.plot import plot_plotly
+import plotly.express as px
+import plotly.offline as py
 import datetime as dt
 
 #####################################################################################################################################
 ### IMPORT DATA ###
 #####################################################################################################################################
-path = r'data' # folder path
-all_files = glob.glob(path + "/*.csv")
+df = pd.read_csv('temp_data.csv')
 
-li = []
-for filename in all_files:
-    df = pd.read_csv(filename, index_col=None, header=0)
-    li.append(df)
-
-df = pd.concat(li, axis=0, ignore_index=True)
+#convert string to date format
+df['Date'] = pd.to_datetime(df['Date'])
 
 #####################################################################################################################################
-### Calculate the forecast data (failing check model from scratch) ###
+### REVIEW THE TIMESERIES DATA ###
 #####################################################################################################################################
-
-
-#####################################################################################################################################
-### ... ###
-#####################################################################################################################################
-
-
+#plot the yield of US Soybean crop
+fig = px.line(df, x='Date', y='Daily minimum temperatures')
+fig.show()
 
 #####################################################################################################################################
-### ... ###
+### Train the Model ###
 #####################################################################################################################################
+
+#format the data to required format for the library {Date: 'DS', Variable: 'Y'}
+df = df.rename(columns={'Date':'ds', 'Daily minimum temperatures': 'Y'})
+forecast_training = df[['ds','Y']]
+
+#fit model
+m = Prophet()
+m.fit(forecast_training)
+
+#####################################################################################################################################
+### Prediction incl. Parameters ###
+#####################################################################################################################################
+
+future = m.make_future_dataframe(periods=365)
+future.tail()
+
+forecast = m.predict(future)
+forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
+
+#####################################################################################################################################
+### REVIEW MODEL PERFORMANCE ###
+#####################################################################################################################################
+
+#plot the forecast
+fig1 = m.plot(forecast)
+
+#plot decomposition of seasonality and trends
+fig2 = m.plot_components(forecast)
+
+#Interactive plots
+py.init_notebook_mode()
+
+fig = plot_plotly(m, forecast)  # This returns a plotly Figure
+py.iplot(fig)
 
 
 #####################################################################################################################################
